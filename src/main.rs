3ec4f599aeb7ae6e3c97333886b14677cf8b65fc
dpirvoiu <adisor.pdw@gmail.com>
2026-01-18 20:30:21 +0200

    initial setup camera and paddle

---
+++ src/main.rs
@@ -0,0 +1,40 @@
+use bevy::math::vec3;
+use bevy::prelude::*;
+
+const PADDLE_START_Y: f32 = 0.0;
+const PADDLE_SIZE: Vec2 = Vec2::new(120.0, 20.0);
+const PADDLE_COLOR: Color = Color::rgb(0.3, 0.3, 0.7);
+
+fn main() {
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .insert_resource(ClearColor(Color::rgb(0.9, 0.9, 0.9)))
+        .add_systems(Update, bevy::window::close_on_esc)
+        .add_systems(Startup, setup)
+        .run();
+}
+
+// This is a Tag component. We use this to identify the player
+#[derive(Component)]
+struct Paddle;
+
+fn setup(mut commands: Commands) {
+    // camera
+    commands.spawn(Camera2dBundle::default());
+
+    commands.spawn((
+        SpriteBundle {
+            transform: Transform {
+                translation: vec3(0.0, PADDLE_START_Y, 0.0),
+                ..Default::default()
+            },
+            sprite: Sprite {
+                color: PADDLE_COLOR,
+                custom_size: Some(PADDLE_SIZE),
+                ..Default::default()
+            },
+            ..Default::default()
+        },
+        Paddle,
+    ));
+}

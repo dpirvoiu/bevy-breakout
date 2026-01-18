c6526aedfeb7969e03f215e1a1800dcaf4a6bf52
dpirvoiu <adisor.pdw@gmail.com>
2026-01-18 21:07:41 +0200

    spawn ball and apply velocity system

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,19 @@
 use bevy::math::vec3;
 use bevy::{prelude::*, time};
 
+// PADDLE
 const PADDLE_START_Y: f32 = 0.0;
 const PADDLE_SIZE: Vec2 = Vec2::new(120.0, 20.0);
 const PADDLE_COLOR: Color = Color::rgb(0.3, 0.3, 0.7);
 const PADDLE_SPEED: f32 = 500.0;
+
+// BALL
+
+const BALL_COLOR: Color = Color::rgb(1.0, 0.5, 0.5);
+const BALL_STARTING_POSITION: Vec3 = Vec3::new(0.0, -5.0, 1.0);
+const BALL_SIZE: Vec2 = Vec2::new(30.0, 30.0);
+const BALL_SPEED: f32 = 400.0;
+const BALL_INITIAL_DIRECTION: Vec2 = Vec2::new(0.5, -0.5);
 
 fn main() {
     App::new()
@@ -12,7 +21,7 @@ fn main() {
         .insert_resource(ClearColor(Color::rgb(0.9, 0.9, 0.9)))
         .add_systems(Update, bevy::window::close_on_esc)
         .add_systems(Startup, setup)
-        .add_systems(FixedUpdate, move_paddle)
+        .add_systems(FixedUpdate, (move_paddle, apply_velocity))
         .run();
 }
 
@@ -20,10 +29,20 @@ fn main() {
 #[derive(Component)]
 struct Paddle;
 
-fn setup(mut commands: Commands) {
+// This is a Tag component. -> BALL
+#[derive(Component)]
+struct Ball;
+
+#[derive(Component, Deref, DerefMut)]
+struct Velocity(Vec2);
+
+fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
     // camera
     commands.spawn(Camera2dBundle::default());
 
+    let ball_tex = asset_server.load("textures/circle.png");
+
+    // Paddle
     commands.spawn((
         SpriteBundle {
             transform: Transform {
@@ -35,9 +54,29 @@ fn setup(mut commands: Commands) {
                 custom_size: Some(PADDLE_SIZE),
                 ..Default::default()
             },
+
             ..Default::default()
         },
         Paddle,
+    ));
+
+    // Ball
+    commands.spawn((
+        SpriteBundle {
+            transform: Transform {
+                translation: BALL_STARTING_POSITION,
+                ..Default::default()
+            },
+            sprite: Sprite {
+                color: BALL_COLOR,
+                custom_size: Some(BALL_SIZE),
+                ..Default::default()
+            },
+            texture: ball_tex,
+            ..Default::default()
+        },
+        Ball,
+        Velocity(BALL_SPEED * BALL_INITIAL_DIRECTION),
     ));
 }
 
@@ -59,4 +98,13 @@ fn move_paddle(
     let new_x =
         paddle_transform.translation.x + direction * PADDLE_SPEED * time_step.period.as_secs_f32();
     paddle_transform.translation.x = new_x;
+}
+
+fn apply_velocity(mut query: Query<(&mut Transform, &Velocity)>, time_step: Res<FixedTime>) {
+    let dt = time_step.period.as_secs_f32();
+
+    for (mut transform, velocity) in &mut query {
+        transform.translation.x += velocity.x * dt;
+        transform.translation.y += velocity.y * dt;
+    }
 }

a292612d657036484707cc9a6dcb1b7a9a5145cd
dpirvoiu <adisor.pdw@gmail.com>
2026-01-19 20:31:34 +0200

    add score

--- src/main.rs
+++ src/main.rs
@@ -36,11 +36,18 @@ const GAP_BETWEEN_BRICKS: f32 = 5.0;
 const GAP_BETWEEN_BRICKS_AND_CEILING: f32 = 20.0;
 const GAP_BETWEEN_BRICKS_AND_SIDES: f32 = 20.0;
 
+// Score
+const SCORE_FONT_SIZE: f32 = 40.0;
+const SCOREBOARD_TEXT_PADDING: Val = Val::Px(5.0);
+const TEXT_COLOR: Color = Color::rgb(0.5, 0.5, 1.0);
+const SCORE_COLOR: Color = Color::rgb(1.0, 0.5, 0.5);
+
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .insert_resource(ClearColor(Color::rgb(0.9, 0.9, 0.9)))
-        .add_systems(Update, bevy::window::close_on_esc)
+        .insert_resource(Scoreboard { score: 0 })
+        .add_systems(Update, (bevy::window::close_on_esc, update_scoreboard))
         .add_systems(Startup, setup)
         .add_systems(
             FixedUpdate,
@@ -79,6 +86,12 @@ struct Brick;
 struct WallBundle {
     sprite_bundle: SpriteBundle,
     collider: Collider,
+}
+
+// Score
+#[derive(Resource, Clone, Copy)]
+struct Scoreboard {
+    score: usize,
 }
 
 fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
@@ -246,6 +259,32 @@ fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
             }
         }
     }
+
+    // SCOREBOARD
+
+    commands.spawn(
+        TextBundle::from_sections([
+            TextSection::new(
+                "Score: ",
+                TextStyle {
+                    font_size: SCORE_FONT_SIZE,
+                    color: TEXT_COLOR,
+                    ..Default::default()
+                },
+            ),
+            TextSection::from_style(TextStyle {
+                font_size: SCORE_FONT_SIZE,
+                color: SCORE_COLOR,
+                ..Default::default()
+            }),
+        ])
+        .with_style(Style {
+            position_type: PositionType::Absolute,
+            top: SCOREBOARD_TEXT_PADDING,
+            left: SCOREBOARD_TEXT_PADDING,
+            ..Default::default()
+        }),
+    );
 }
 
 fn move_paddle(
@@ -284,6 +323,7 @@ fn apply_velocity(mut query: Query<(&mut Transform, &Velocity)>, time_step: Res<
 
 fn check_ball_collision(
     mut commands: Commands,
+    mut score: ResMut<Scoreboard>,
     mut ball_query: Query<(&mut Velocity, &Transform, &Ball)>,
     collider_query: Query<(Entity, &Transform, &Collider, Option<&Brick>)>,
 ) {
@@ -317,9 +357,17 @@ fn check_ball_collision(
                 }
 
                 if opt_brick.is_some() {
+                    score.score += 1;
+                    println!("Score {}", score.score);
+
                     commands.entity(other_entity).despawn();
                 }
             }
         }
     }
+}
+
+fn update_scoreboard(score: Res<Scoreboard>, mut query: Query<&mut Text>) {
+    let mut text = query.single_mut();
+    text.sections[1].value = score.score.to_string();
 }
